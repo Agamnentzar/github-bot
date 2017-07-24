14a5c1609107ab0a686262f78d6e499f960118f8
Radosław Eichler <agamnentzar@gmail.com>
2017-07-25 01:15:18 +0200

    Switch to webhook handler

--- bot.js
+++ bot.js
@@ -1,63 +1,32 @@
-const express = require('express');
+const http = require('http');
+const createHandler = require('github-webhook-handler');
 const bodyParser = require('body-parser');
 const githubApi = require('github');
 const config = require('./config.json');
 const github = githubApi({});
+const handler = createHandler({ path: '/', secret: config.secret });
 
-github.authenticate({
-  type: "oauth",
-  token: config.token,
-});
+github.authenticate({ type: "oauth", token: config.token });
 
-// github.issues.getForRepo({
-//   owner: 'Agamnentzar',
-//   repo: 'release-test',
-// })
-//   .then(x => x.data.map(i => i.body))
-//   .then(x => console.log(x))
-//   .catch(e => console.error(e));
-
-// github.issues.createComment({
-//   owner: 'Agamnentzar',
-//   repo: 'release-test',
-//   number: 1,
-//   body: '• c •'
-// })
-//   .then(x => console.log(x))
-//   .catch(e => console.error(e));
-
-const app = express();
-
-app.set('port', process.env.PORT || 8095);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(require('errorhandler')());
-
-app.get('*', handleRequest);
-app.post('*', handleRequest);
-
-app.listen(app.get('port'), () => console.log('Listening on port ' + app.get('port')));
+http.createServer(handleRequest).listen(process.env.PORT || 8095);
 
 function handleRequest(req, res) {
-  console.log('------ request ------');
-  console.log('METHOD: ', req.method);
-  console.log('PATH: ', req.path);
-  console.log('BODY: ', req.body);
-  console.log('HEADERS: ', req.headers);
-  res.send('');
-  
-  if (req.body.action) {
-    handleEvent(req.body);
-  }
+  handler(req, res, err => res.send('error'));
 }
 
-function handleEvent(body) {
-  if (body.action === 'created' && body.repository && body.issue && body.comment) {
-    github.issues.editComment({
-      owner: body.repository.owner.login,
-      repo: body.repository.name,
-      id: body.comment.id,
-      body: body.comment.body + ' • c •',
-    }).catch(e => console.error(e));
-  }
+handler.on('error', err => console.error(err));
+handler.on('issue_comment', event => handleComment(event.payload));
+
+function handleComment(body) {
+  github.issues.editComment({
+    owner: body.repository.owner.login,
+    repo: body.repository.name,
+    id: body.comment.id,
+    body: body.comment.body + ' • c •',
+  }).catch(e => console.error(e));
 }
+
+//const events = require('github-webhook-handler/events');
+//Object.keys(events).forEach(e => console.log(e, '=', events[e]));
+
+console.log('started');
